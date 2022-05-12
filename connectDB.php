3debc66e5602905fc1c3f4b75cbae9cb7fab7156
Esraa Abo Eleneen <88058192+EsraaAboEleneen@users.noreply.github.com>
2022-05-12 03:32:46 +0200

    Delete connectDB.php

--- connectDB.php
+++
@@ -1,8 +0,0 @@
-<?php
-$hostname = 'localhost';
-$username = 'root';
-$password = '';
-$pdoDB = new PDO("mysql:host=$hostname;dbname=product_crud", $username, $password);
-$pdoDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//this if i use the first way at identifing $pdoDB
-//return $pdoDB;
